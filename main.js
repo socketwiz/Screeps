0cc3dc87e681ef2426f4dcbe95533cb6597b42a9
Ricky Nelson <ricky.nelson@invincea.com>
2017-04-22 14:34:17 -0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,86 @@
+
+var roleHarvester = require('role.harvester');
+var roleUpgrader = require('role.upgrader');
+var roleBuilder = require('role.builder');
+
+function formatError(error) {
+    switch (error) {
+        case ERR_NOT_ENOUGH_ENERGY:
+            return 'Not enough energy';
+        default:
+            return error;
+    }
+}
+
+module.exports.loop = function () {
+    var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
+    var upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
+    var builders = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
+
+    console.log('Harvesters: ' + harvesters.length);
+    console.log('Upgrades: ' + upgraders.length);
+    console.log('Builders: ' + builders.length);
+
+    // var tower = Game.getObjectById('d78e235dfa65938db402f4f0');
+    var tower = false;
+
+    if (tower) {
+        var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+        var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
+            'filter': (structure) => structure.hits < structure.hitsMax
+        });
+
+        if (closestDamagedStructure) {
+            tower.repair(closestDamagedStructure);
+        }
+
+        if (closestHostile) {
+            tower.attack(closestHostile);
+        }
+    }
+
+    for (var name in Memory.creeps) {
+        if (!Game.creeps[name]) {
+            delete Memory.creeps[name];
+            console.log('Clearing non-existing creep memory:', name);
+        }
+    }
+
+    if (harvesters.length < 2) {
+        // var newHarvester = Game.spawns['SpawnDominator'].createCreep([WORK, WORK, WORK, WORK, CARRY, MOVE, MOVE], undefined, {role: 'harvester'});
+        var newHarvester = Game.spawns['SpawnDominator'].createCreep([WORK, CARRY, MOVE, MOVE], undefined, {role: 'harvester'});
+        console.log('Spawning new harvester: ' + formatError(newHarvester));
+    }
+
+    if (upgraders.length < 3) {
+        var newUpgrader = Game.spawns['SpawnDominator'].createCreep([WORK, CARRY, MOVE, MOVE], undefined, {role: 'upgrader'});
+
+        console.log('Spawning new upgrader: ' + formatError(newUpgrader));
+    }
+
+    if (builders.length < 2) {
+        var newBuilder = Game.spawns['SpawnDominator'].createCreep([WORK, CARRY, MOVE, MOVE], undefined, {role: 'builder'});
+        console.log('Spawning new builder: ' + formatError(newBuilder));
+    }
+
+    for (var room in Game.rooms) {
+        console.log('Room "' + room + '" has ' + Game.rooms[room].energyAvailable + ' energy');
+    }
+
+    for(var creeps in Game.creeps) {
+        var creep = Game.creeps[creeps];
+
+        if (creep.memory.role == 'harvester') {
+            roleHarvester.run(creep);
+        }
+        if (creep.memory.role == 'upgrader') {
+            roleUpgrader.run(creep);
+        }
+        if (creep.memory.role == 'builder') {
+            roleBuilder.run(creep);
+        }
+    }
+};
+
+// Game.spawns['SpawnDominator'].room.controller.activateSafeMode();
+// Game.spawns['SpawnDominator'].room.createConstructionSite( 23, 22, STRUCTURE_TOWER );
