ec7bef2b6192e73602114579ddf2a11cc3d1dfc4
Ricky Nelson <ricky.nelson@invincea.com>
2017-05-03 07:20:19 -0400

    Creeps still had fatigue

--- data.js
+++ data.js
@@ -25,28 +25,28 @@ const DATA = {
                         'count': 2,
                         'features': {
                             'needed': [{WORK: 1}, {CARRY: 1}, {MOVE: 3}],
-                            'wanted': [{TOUGH: 10}, {WORK: 4}, {CARRY: 2}, {MOVE: 6}, {ATTACK: 4}, {MOVE: 1}]
+                            'wanted': [{TOUGH: 13}, {WORK: 4}, {CARRY: 2}, {MOVE: 7}, {ATTACK: 3}, {MOVE: 1}]
                         },
                         'role': 'harvester'
                     },
                     {
                         'count': 3,
                         'features': {
-                            'wanted': [{TOUGH: 10}, {WORK: 4}, {CARRY: 2}, {MOVE: 6}, {RANGED_ATTACK: 2}, {MOVE: 1}]
+                            'wanted': [{TOUGH: 5}, {WORK: 4}, {CARRY: 2}, {MOVE: 7}, {RANGED_ATTACK: 2}, {MOVE: 1}]
                         },
                         'role': 'upgrader'
                     },
                     {
                         'count': 1,
                         'features': {
-                            'wanted': [{TOUGH: 10}, {WORK: 4}, {CARRY: 2}, {MOVE: 6}, {ATTACK: 4}, {MOVE: 1}]
+                            'wanted': [{TOUGH: 13}, {WORK: 4}, {CARRY: 2}, {MOVE: 7}, {ATTACK: 3}, {MOVE: 1}]
                         },
                         'role': 'builder'
                     },
                     {
                         'count': 2,
                         'features': {
-                            'wanted': [{TOUGH: 20}, {WORK: 4}, {CARRY: 2}, {MOVE: 6}, {HEAL: 1}, {MOVE: 1}]
+                            'wanted': [{TOUGH: 10}, {WORK: 4}, {CARRY: 2}, {MOVE: 8}, {HEAL: 1}, {MOVE: 1}]
                         },
                         'role': 'roadCrew'
                     }
